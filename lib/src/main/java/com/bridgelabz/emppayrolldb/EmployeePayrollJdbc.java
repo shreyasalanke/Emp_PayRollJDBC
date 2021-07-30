338920f1cf170e03d2f989248add88f5ff7f06e7
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-30 17:25:40 +0530

    Initial Commit: Welcome to emp_payroll JDBC program

---
+++ lib/src/main/java/com/bridgelabz/emppayrolldb/EmployeePayrollJdbc.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.emppayrolldb;
+
+public class EmployeePayrollJdbc 
+{
+	public static void main(String[] args) 
+	{
+	 System.out.println("Welcome to Emp_payrollJDBC Program");
+	}	
+}
