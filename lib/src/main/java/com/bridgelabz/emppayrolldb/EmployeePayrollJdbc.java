d56718234b0d6e287f4fb0a0c534cc0435d87fa5
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-30 17:37:06 +0530

    ADDED: program to create emp_payroll program and establish the connection

--- lib/src/main/java/com/bridgelabz/emppayrolldb/EmployeePayrollJdbc.java
+++ lib/src/main/java/com/bridgelabz/emppayrolldb/EmployeePayrollJdbc.java
@@ -1,9 +1,35 @@
 package com.bridgelabz.emppayrolldb;
 
-public class EmployeePayrollJdbc 
-{
-	public static void main(String[] args) 
-	{
-	 System.out.println("Welcome to Emp_payrollJDBC Program");
-	}	
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+
+public class EmployeePayrollJdbc {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		ListDrivers();
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp_payroll_service?useSSL=false", "root", "ShreyaS@18");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Connection Done..!!!");
+	}
+
+	private static void ListDrivers() {
+		Enumeration<Driver> driverList=DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driverClass=(Driver)driverList.nextElement();
+			System.out.println("  "+driverClass.getClass().getName());
+		}
+	}
+	
+
 }
